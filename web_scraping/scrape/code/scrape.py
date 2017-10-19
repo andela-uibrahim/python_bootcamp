f660e8bc8dccc0b89e665d0b19b508ccc6c88d08
andela-uibrahim <usman.ibrahim@andela.com>
2017-10-19 16:28:08 -0500

    included web scraping

--- web_scraping/scrape/code/scrape.py
+++ web_scraping/scrape/code/scrape.py
@@ -2,73 +2,58 @@ import requests
 from bs4 import BeautifulSoup
 
 base_url = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc='
-loc = 'Newport+Beach,+CA'
-page = 10
+loc = 'Newport+Beach,+CA,+United+States'
+current_page = 0
 
-
-
-url = base_url + loc + "&start=" + str(page)
-
-
-yelp_r = requests.get(url)
-yelp_soup = BeautifulSoup(yelp_r.text, 'html.parser')
-
-
-businesses = yelp_soup.findAll('div', {'class': 'biz-listing-large'})
-
-for biz in businesses:
-    #print(biz)
-    title = biz.findAll('a', {'class': 'biz-name'})[0].text
-    print(title)
-    address = biz.findAll('address')[0] #.replace(' ', '')
-    print(address)
-    print('\n')
-    phone = biz.findAll('span', {'class': 'biz-phone'})[0].text
-    print(phone)
-
-
-
-file_path = 'yelp-{loc}.txt'.format(loc=loc)
-
-with open(file_path, "a") as textfile:
+while current_page < 201:
+    print(current_page)
+    url = base_url + loc + "&start=" + str(current_page)
+    yelp_r = requests.get(url)
+    yelp_soup = BeautifulSoup(yelp_r.text, 'html.parser')
     businesses = yelp_soup.findAll('div', {'class': 'biz-listing-large'})
-    for biz in businesses:
-        #print(biz)
-        title = biz.findAll('a', {'class': 'biz-name'})[0].text
-        print(title)
-        address = biz.findAll('address')[0] #.replace(' ', '')
-        print(address)
-        print('\n')
-        phone = biz.findAll('span', {'class': 'biz-phone'})[0].text
-        print(phone)
-        page_line = "{title}\n{address}\n{phone}\n\n".format(
-                title=title,
-                address=address,
-                phone = phone
-            )
-        textfile.write(page_line)
+    file_path = 'yelp-{loc}-2.txt'.format(loc=loc)
+    with open(file_path, "a") as textfile:
+        businesses = yelp_soup.findAll('div', {'class': 'biz-listing-large'})
+        for biz in businesses:
+            #print(biz)
+            title = biz.findAll('a', {'class': 'biz-name'})[0].text
+            print(title)
+            second_line = ""
+            first_line = ""
+            try:
+                address = biz.findAll('address')[0].contents
+                for item in address:
+                    if "br" in str(item):
+                        #print(item.getText())
+                        second_line += item.getText().strip(" \n\t\r")
+                    else:
+                        #print(item.strip(" \n\t\r"))
+                        first_line = item.strip(" \n\t\r")
+                print(first_line)
+                print(second_line)
+            except:
+                pass
+            print('\n')
+            try:
+                phone = biz.findAll('span', {'class': 'biz-phone'})[0].getText().strip(" \n\t\r")
+            except:
+                phone = None
+            print(phone)
+            page_line = "{title}\n{address_1}\n{address_2}\n{phone}\n\n".format(
+                    title=title,
+                    address_1=first_line,
+                    address_2=second_line,
+                    phone = phone
+                )
+            textfile.write(page_line)
+    current_page += 10
 
 
 
-
-
-
-print(yelp_soup.findAll('li', {'class': 'regular-search-result'}))
-
-
-print(yelp_soup.findAll('a', {'class': 'biz-name'}))
-
-for name in yelp_soup.findAll('a', {'class': 'biz-name'}):
-    print(name.text)
-
-
-
-
-
-print(yelp_r.status_code) #200
-
-print(yelp_soup.prettify())
-print(yelp_soup.findAll('a', {}))
-
-for link in yelp_soup.findAll('a'):
-    print(link)
+'''
+Working with Django
+obj = SomeModel()
+obj.title = title
+obj.line_1 = first_line
+obj.save()
+'''
